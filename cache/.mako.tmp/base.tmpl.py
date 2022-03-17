ac8b42db4fda629c31601bd548da4e1bd34c5646
Pablo Cervantes <cervantes@ks.c.titech.ac.jp>
2022-03-18 00:13:51 +0900

    Nikola auto commit.
    Nikola version: 8.2.0

--- cache/.mako.tmp/base.tmpl.py
+++ cache/.mako.tmp/base.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1647529675.7709138
+_modified_time = 1647530031.2818303
 _enable_loop = True
 _template_filename = 'themes/lanyon/templates/base.tmpl'
 _template_uri = 'base.tmpl'
@@ -37,24 +37,24 @@ def render_body(context,**pageargs):
         _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'header')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'footer')._populate(_import_ns, ['*'])
-        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
-        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
-        js_date_format = _import_ns.get('js_date_format', context.get('js_date_format', UNDEFINED))
-        lanyon_subtheme = _import_ns.get('lanyon_subtheme', context.get('lanyon_subtheme', UNDEFINED))
+        momentjs_locales = _import_ns.get('momentjs_locales', context.get('momentjs_locales', UNDEFINED))
         def extra_js():
             return render_extra_js(context._locals(__M_locals))
-        header = _mako_get_namespace(context, 'header')
-        lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
+        footer = _mako_get_namespace(context, 'footer')
+        body_end = _import_ns.get('body_end', context.get('body_end', UNDEFINED))
         base = _mako_get_namespace(context, 'base')
+        set_locale = _import_ns.get('set_locale', context.get('set_locale', UNDEFINED))
+        lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
+        date_fanciness = _import_ns.get('date_fanciness', context.get('date_fanciness', UNDEFINED))
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
+        header = _mako_get_namespace(context, 'header')
+        js_date_format = _import_ns.get('js_date_format', context.get('js_date_format', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
         def content():
             return render_content(context._locals(__M_locals))
-        body_end = _import_ns.get('body_end', context.get('body_end', UNDEFINED))
-        momentjs_locales = _import_ns.get('momentjs_locales', context.get('momentjs_locales', UNDEFINED))
-        footer = _mako_get_namespace(context, 'footer')
-        set_locale = _import_ns.get('set_locale', context.get('set_locale', UNDEFINED))
-        date_fanciness = _import_ns.get('date_fanciness', context.get('date_fanciness', UNDEFINED))
+        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
+        lanyon_subtheme = _import_ns.get('lanyon_subtheme', context.get('lanyon_subtheme', UNDEFINED))
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
--- cache/.mako.tmp/listing.tmpl.py
+++ cache/.mako.tmp/listing.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1647529675.7613358
+_modified_time = 1647530031.2718496
 _enable_loop = True
 _template_filename = '/home/pablo/.virtualenvs/nikola/lib/python3.8/site-packages/nikola/data/themes/base/templates/listing.tmpl'
 _template_uri = 'listing.tmpl'
@@ -32,14 +32,14 @@ def render_body(context,**pageargs):
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         crumbs = context.get('crumbs', UNDEFINED)
         source_link = context.get('source_link', UNDEFINED)
-        code = context.get('code', UNDEFINED)
-        messages = context.get('messages', UNDEFINED)
-        ui = _mako_get_namespace(context, 'ui')
-        title = context.get('title', UNDEFINED)
-        folders = context.get('folders', UNDEFINED)
         files = context.get('files', UNDEFINED)
+        folders = context.get('folders', UNDEFINED)
+        messages = context.get('messages', UNDEFINED)
+        code = context.get('code', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
+        ui = _mako_get_namespace(context, 'ui')
+        title = context.get('title', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
@@ -58,14 +58,14 @@ def render_content(context,**pageargs):
     try:
         crumbs = context.get('crumbs', UNDEFINED)
         source_link = context.get('source_link', UNDEFINED)
-        code = context.get('code', UNDEFINED)
-        messages = context.get('messages', UNDEFINED)
-        ui = _mako_get_namespace(context, 'ui')
-        title = context.get('title', UNDEFINED)
-        folders = context.get('folders', UNDEFINED)
         files = context.get('files', UNDEFINED)
+        folders = context.get('folders', UNDEFINED)
+        messages = context.get('messages', UNDEFINED)
+        code = context.get('code', UNDEFINED)
         def content():
             return render_content(context)
+        ui = _mako_get_namespace(context, 'ui')
+        title = context.get('title', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer(str(ui.breadcrumbs(crumbs)))
